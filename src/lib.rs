69151ae6ef18ec43a77971a1bac897683bb0ccb2
Jacek Bilski <jacek@bilski.tech>
2023-06-05 15:06:22 +0200

    Not exactly working, but also not failing miserably. Still not trying to show anything.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,83 @@
 extern crate console_error_panic_hook;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use web_sys::{console, HtmlCanvasElement};
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    window::{WindowBuilder},
 };
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn get_canvas() -> HtmlCanvasElement {
+    let document = window().document().unwrap();
+    let canvas = document.get_element_by_id("webgpu-canvas").unwrap();
+    let canvas: HtmlCanvasElement = canvas.dyn_into::<HtmlCanvasElement>().expect("Counldn't find canvas element");
+    canvas
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub async fn main_js() -> Result<(), JsValue> {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    use winit::platform::web::WindowBuilderExtWebSys;
+    let window = WindowBuilder::new().with_canvas(Some(get_canvas())).build(&event_loop).unwrap();
+
+    let size = window.inner_size();
+
+    // The instance is a handle to our GPU
+    // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(),
+        dx12_shader_compiler: Default::default(),
+    });
+
+    // # Safety
+    //
+    // The surface needs to live as long as the window that created it.
+    // State owns the window so this should be safe.
+    let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+    let adapter = instance.request_adapter(
+        &wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        },
+    ).await.unwrap();
+
+    let (device, queue) = adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            features: wgpu::Features::empty(),
+            limits: wgpu::Limits::downlevel_webgl2_defaults(), // should be wgpu::Limits::default()
+            label: None,
+        },
+        None, // Trace path
+    ).await.unwrap();
+
+    let surface_caps = surface.get_capabilities(&adapter);
+    // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+    // one will result all the colors coming out darker. If you want to support non
+    // sRGB surfaces, you'll need to account for that when drawing to the frame.
+    let surface_format = surface_caps.formats.iter()
+        .copied()
+        .find(|f| f.is_srgb())
+        .unwrap_or(surface_caps.formats[0]);
+    let config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: surface_format,
+        width: size.width,
+        height: size.height,
+        present_mode: surface_caps.present_modes[0],    // PresentMode::Fifo or AutoVSync
+        alpha_mode: surface_caps.alpha_modes[0],
+        view_formats: vec![],
+    };
+    surface.configure(&device, &config);
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
